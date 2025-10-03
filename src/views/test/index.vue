a828e075711147233bc38af64eb7eaaaea54d012
pqcqaq <905739777@qq.com>
2025-10-03 19:03:18 +0800

    feat: 新增测试页面

---
+++ src/views/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button type="primary" @click="handleClick">
+      测试发送socket消息
+    </el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { sendUserSocketMsg } from "qc-admin-api-common/test";
+
+const handleClick = () => {
+  sendUserSocketMsg().then(() => {
+    console.log("发送成功");
+  });
+};
+</script>
+
+<style scoped></style>
